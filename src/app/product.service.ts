7eac9b39d83535767637fa7b56b74a5bfba62de4
Mikisho <mikiye.82@gmail.com>
2018-12-23 18:08:30 -0600

    product_Service_added

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -8,10 +8,6 @@ export class ProductService {
 
   constructor(private _http: Http) {}
   getAlbum(id: number) {
-    return this._http.get(this._albumUrl).pipe(
-      map(response => {
-        response.json();
-      })
-    );
+    return this._http.get(this._albumUrl).pipe(map(response => response.json()));
   }
 }
